80ba28c05fdbb3dc77461c4854037091130f0498
Arhamabdullah <92443842+Arhamabdullah@users.noreply.github.com>
2024-05-14 23:07:46 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -131,12 +131,12 @@ const Home: NextPage = () => {
         </Head>
         <header className="flex justify-between items-center px-4 py-2">
           <div className="flex items-center">
-            <Image src="/website_logo.png" alt="Left Logo" width={50} height={50} />
+{/*             <Image src="/website_logo.png" alt="Left Logo" width={50} height={50} /> */}
             <h2 className="ml-2 text-4xl font-bold">To-do</h2>
           </div>
-          <div>
+{/*           <div>
             <Image src="/O_LOGO.png" alt="Right Logo" width={50} height={50} />
-          </div>
+          </div> */}
         </header>
         <div className="flex flex-col items-center min-h-screen pt-10">
           <InputField
