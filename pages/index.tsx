487d3f86f40aee6cbd4a188e9401aa83a45c0654
Arhamabdullah <92443842+Arhamabdullah@users.noreply.github.com>
2024-05-14 22:53:26 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image' // Add this import
-
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { DragDropContext, DropResult } from 'react-beautiful-dnd'
-
 import InputField from '../components/inputfield'
 import Todos from '../components/todos'
-
-import { Status, Todo, TodosStatus, TodosView} from '../models/todo'
+import { Status, Todo, TodosView, TodosStatus } from '../models/todo'
 import styles from '../styles/Home.module.css'
-import { setDoc,doc,addDoc,collection, deleteDoc, query, onSnapshot, QuerySnapshot } from 'firebase/firestore'
+import { setDoc, doc, addDoc, collection, deleteDoc, query, onSnapshot, QuerySnapshot } from 'firebase/firestore'
 import { db } from '../backend/firebase'
 
 const Home: NextPage = () => {
@@ -19,9 +16,7 @@ const Home: NextPage = () => {
   const [backlogTodos, setBacklogTodos] = useState<Todo[]>([])
   const [activeTodos, setActiveTodos] = useState<Todo[]>([])
   const [completedTodos, setCompletedTodos] = useState<Todo[]>([])
- 
- 
- 
+
   useEffect(() => {
     let backlogTodos = window.localStorage.getItem('backlogTodos')
     if (backlogTodos) {
@@ -40,45 +35,42 @@ const Home: NextPage = () => {
     }
   }, [])
 
- 
-
-  const addNewTodo =async (e: React.FormEvent) => {
+  const addNewTodo = async (e: React.FormEvent) => {
     e.preventDefault()
     if (name) {
       const newTodo = {
         id: Date.now(),
-        todoname:name,
+        todoname: name,
         status: Status.Backlog,
         isDone: false
       }
 
       setBacklogTodos([...backlogTodos, newTodo])
-      
+
       try {
         // Add new todo to Firestore
-        const docRef =collection(db,'todos') 
-        await addDoc(docRef,newTodo)
-      
-     //   newTodo.id = docRef.id; 
+        const docRef = collection(db, 'todos')
+        await addDoc(docRef, newTodo)
         setBacklogTodos([...backlogTodos, newTodo]);
         setName('');
       } catch (error) {
         console.error('Error adding document: ', error);
       }
-     }
+    }
   }
-useEffect(()=>{
-  const  q = query(collection(db, 'todos'))
-  const unsubscribe = onSnapshot(q,(QuerySnapshot)=>{
-    let todosArr:any = []
-    QuerySnapshot.forEach((doc)=>{
-      todosArr.push({...doc.data(),id: doc.id})
-    });
-    setBacklogTodos(todosArr)
-  })
-  return() =>unsubscribe()
-},[])
-  
+
+  useEffect(() => {
+    const q = query(collection(db, 'todos'))
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let todosArr: any = []
+      QuerySnapshot.forEach((doc) => {
+        todosArr.push({ ...doc.data(), id: doc.id })
+      });
+      setBacklogTodos(todosArr)
+    })
+    return () => unsubscribe()
+  }, [])
+
   const onDragEndHandler = (result: DropResult) => {
     const { destination, source } = result
 
@@ -102,8 +94,6 @@ useEffect(()=>{
         add = complete[source.index]
         complete.splice(source.index, 1)
         break
-
-      
     }
 
     if (add) {
@@ -117,8 +107,6 @@ useEffect(()=>{
         case TodosStatus.CompletedTodos:
           complete.splice(destination.index, 0, add)
           break
-
-        
       }
     }
 
@@ -126,15 +114,12 @@ useEffect(()=>{
     setActiveTodos(active)
     setCompletedTodos(complete)
 
-    
     if (window) {
       window.localStorage.setItem('backlogTodos', JSON.stringify(backlog))
       window.localStorage.setItem('activeTodos', JSON.stringify(active))
       window.localStorage.setItem('completedTodos', JSON.stringify(complete))
-
-       }
+    }
   }
-
 
   return (
     <DragDropContext onDragEnd={onDragEndHandler}>
@@ -150,11 +135,17 @@ useEffect(()=>{
             <h2 className="ml-2 text-4xl font-bold">To-do</h2>
           </div>
           <div>
-            <Image src="/O_LOGO.png" alt="Right Logo" width={50} height={50} /> 
+            <Image src="/O_LOGO.png" alt="Right Logo" width={50} height={50} />
           </div>
         </header>
         <div className="flex flex-col items-center min-h-screen pt-10">
-          {/* Rest of your content */}
+          <InputField
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            onSubmit={addNewTodo}
+            placeholder="Add a new todo"
+          />
+          {/* You can add more content here */}
         </div>
         <Todos
           view={view}
@@ -176,7 +167,3 @@ Home.getInitialProps = async ({ }) => {
 }
 
 export default Home
-
-
-
-
